ea15c417acf9c2144efe12a2295e84704b57384b
Albert Conceição <developer.albert@outlook.com>
2022-06-22 11:52:16 -0300

    fix: responsividade

--- src/components/BoxContent/index.tsx
+++ src/components/BoxContent/index.tsx
@@ -9,7 +9,7 @@ interface BoxContentProps {
 
 export const BoxContent = ({width,height, children }: BoxContentProps) => {
     return (
-        <Box maxWidth={`${width}`} maxH={`${height}`} mx="auto" textAlign={"center"} bg="gray.200" boxShadow={["2xl"]} borderRadius="10px" p="6">
+        <Box maxWidth={`${width}`} maxH={`${height}`} mx="auto" textAlign={"center"} bg="gray.200" boxShadow={["2xl"]} borderRadius="10px" p="6" m="4">
             {children}
         </Box>
     );
