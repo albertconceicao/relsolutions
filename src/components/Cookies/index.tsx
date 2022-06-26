339e25115033e848e9492538a812433295e5f492
Albert Conceição <developer.albert@outlook.com>
2022-06-25 21:28:14 -0300

    feat: adicionando aviso de cookies no site

---
+++ src/components/Cookies/index.tsx
@@ -0,0 +1,18 @@
+import { Text } from "@chakra-ui/react";
+import CookieConsent from "react-cookie-consent";
+
+export const Cookies = () => {
+    return (
+        <CookieConsent
+        location="bottom"
+        buttonText="Entendi e fechar"
+        cookieName="myAwesomeCookieName2"
+        style={{ background: "#ED7526", opacity: "0.8"}}
+        buttonStyle={{ color: "white", fontSize: "1.1rem", background: "#424141", marginRight: "8rem" }}
+        expires={1}
+        debug={true}
+        >
+            <Text fontSize="1.3rem">Este website utiliza cookies para melhorar sua experiência.{" "}</Text>
+        </CookieConsent>
+    );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { Header } from '../components/Header';
 import { WhatsApp } from '../components/WhatsApp';
 import { theme } from '../styles/theme';
+import { Cookies } from '../components/Cookies';
+
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -9,6 +11,7 @@ function MyApp({ Component, pageProps }) {
       <Header />
       <Component {...pageProps} />
       <WhatsApp />
+      <Cookies />
     </ChakraProvider>
   );
 }
